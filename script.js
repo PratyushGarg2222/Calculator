b502086f7332f4e6778e2a6cf2ad5e8b1b50496d
PratyushGarg2222 <pratyush.garg2222@gmail.com>
2020-05-13 01:32:09 +0530

    Added the styling and looks

---
+++ script.js
@@ -0,0 +1,13 @@
+let buttonsList = document.querySelectorAll(".button");
+
+var resultText = document.getElementById("resultText");
+
+buttonsList[0].addEventListener("click", function(){
+
+
+    
+    resultText.textContent = '';
+
+
+
+})
